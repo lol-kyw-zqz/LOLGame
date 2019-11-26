78af69d75bf61fbb3f73030a0f54c3519fc3c358
kyw <1337441734@qq.com>
2019-11-26 22:26:44 +0800

    修改dao层代码

--- src/main/java/com/kyw/lolgames/dao/HeroDao.java
+++ src/main/java/com/kyw/lolgames/dao/HeroDao.java
@@ -12,69 +12,6 @@ import java.util.List;
  */
 public class HeroDao {
 
-//    private Connection connection = null;
-//    private PreparedStatement preparedStatement = null;
-//    private ResultSet resultSet = null;
-
-//    public HerosEntity queryHeroById(int id){
-//        HerosEntity entity = null;
-//        String sql = "select * from heros where id=?";
-//        connection = DBUtil.getConnection();
-//
-//        try {
-//            preparedStatement = connection.prepareStatement(sql);
-//            preparedStatement.setInt(1,id);
-//            resultSet = preparedStatement.executeQuery();
-//            while (resultSet.next()){
-//                entity = new HerosEntity();
-//                resultToVO(entity,resultSet);
-//            }
-//
-//        }catch (Exception e){
-//            e.printStackTrace();
-//        }finally {
-//            DBUtil.close(resultSet,preparedStatement,connection);
-//        }
-//        return entity;
-//    }
-//
-//    public List<HerosEntity> quereyAllHero(){
-//        List<HerosEntity> resList = new ArrayList<>();
-//        HerosEntity entity;
-//        String sql = "select * from heros";
-//        connection = DBUtil.getConnection();
-//
-//        try {
-//            preparedStatement = connection.prepareStatement(sql);
-//            resultSet = preparedStatement.executeQuery();
-//            while (resultSet.next()){
-//                entity = new HerosEntity();
-//                resultToVO(entity,resultSet);
-//                resList.add(entity);
-//            }
-//
-//        }catch (Exception e){
-//            e.printStackTrace();
-//        }finally {
-//            DBUtil.close(resultSet,preparedStatement,connection);
-//        }
-//        return resList;
-//    }
-//
-//    private void resultToVO(HerosEntity entity,ResultSet resultSet) throws Exception{
-//        entity.setId(resultSet.getInt("id"));
-//        entity.setName(resultSet.getString("name"));
-//        entity.setHp(resultSet.getFloat("hp"));
-//        entity.setMp(resultSet.getFloat("mp"));
-//        entity.setAd(resultSet.getFloat("ad"));
-//        entity.setCritRate(resultSet.getDouble("critRate"));
-//        entity.setSkillDescription(resultSet.getString("skillDescription"));
-//        entity.setSkillsVal(resultSet.getFloat("skillsVal"));
-//        entity.setSkillCost(resultSet.getFloat("skillCost"));
-//        entity.setArmor(resultSet.getFloat("armor"));
-//        entity.setMagicResistance(resultSet.getFloat("magicResistance"));
-//    }
-
     public HerosEntity queryHeroById(int id){
         HerosEntity entity;
         String sql = "select * from heros where id=?";
